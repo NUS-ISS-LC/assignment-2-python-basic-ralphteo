744292b54627ffc31369ee5578599f05022aa1d8
Teo Chee Wee <e1583288@u.nus.edu>
2025-11-09 16:30:57 +0000

    Assignment2_Teo_Chee_Wee

--- ex1.py
+++ ex1.py
@@ -1,3 +1,11 @@
-def find(s, n):
-# write your implementation here
+def search_match(arrayl, n):
+    for i in range(len(arrayl)):
+        for j in range(i+1, len(arrayl)):
+            if arrayl[i]+ arrayl[j] == n:
+                return [array[i], array[j]]
     return None
+
+inputarray = input("Enter array with at least 3-4 numbers(comma separated): ")
+array = [int(x) for x in inputarray.split(",")]
+target = int(input("Enter the target sum: "))
+print(f"the integer pair for the target is: ", search_match(array,target))
